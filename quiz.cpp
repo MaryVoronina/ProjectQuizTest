65799528c78af1f703428a98d60cbccf2465854e
MaryVoronina <voroninamary106@gmail.com>
2025-12-14 20:47:08 +0200

    Update quiz.cpp

--- quiz.cpp
+++ quiz.cpp
@@ -2,11 +2,14 @@
 using namespace System;
 using namespace System::Windows::Forms;
 using namespace ProjectQuizTest;
-[STAThread]
+
+
+[STAThreadAttribute]
 int main(array<System::String^>^ args)
 {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
+	ProjectQuizTest::quiz form;
 	Application::Run(gcnew quiz());
 
 	return 0;
